0a57e30ef8937efc313ec6a6a3454fdb264802ce
Dandelion <51200196+Dandelionss@users.noreply.github.com>
2019-06-16 18:46:38 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -4,33 +4,13 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
-    'aliyun-hk': {
-      label: '轻量云-香港',
-      lines: [
-        // 多条线路，负载均衡系统会从其中选一条
-        'node-aliyun-hk-0.etherdream.com:8443',
-        'node-aliyun-hk-1.etherdream.com:8443'
-      ]
-    },
-    'aliyun-sg': {
-      label: '轻量云-新加坡',
-      lines: [
-        'node-aliyun-sg.etherdream.com:8443'
-      ]
-    },
-    'bwh-la': {
-      label: '搬瓦工-洛杉矶',
-      lines: [
-        'node-bwh-la.etherdream.com:8443'
-      ]
-    },
     'cfworker': {
       label: 'Cloudflare Worker',
       hidden: true,
       lines: [
         // 实验中...
         // 参考 https://github.com/EtherDream/jsproxy/tree/master/cf-worker
-        'node-cfworker.etherdream.com:8443'
+        'js.qixiv.me:8443'
       ]
     }
   },
@@ -38,7 +18,7 @@ jsproxy_config({
   /**
    * 默认节点  
    */
-  node_default: 'aliyun-hk',
+  node_default: 'cfworker',
 
   /**
    * 加速节点
